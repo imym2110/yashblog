a94f707d66a1c6cad30ecb68ced97f2b10ea7d31
maniar yash <maniaryash21@gmail.com>
2023-04-19 18:04:22 +0530

    created Addblog Form updates the services.

--- src/app/admin/addblog/addblog.component.ts
+++ src/app/admin/addblog/addblog.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { StrapiService } from 'src/app/services/strapi.service';
 
 
 @Component({
@@ -8,4 +10,51 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./addblog.component.scss']
 })
 export class AddblogComponent {
+  blogdetails:any[]=[];
+  mydata:any[]=[];
+  public addblogform: FormGroup;
+  category: any;
+
+  constructor(private detail: FormBuilder, private StrapiService: StrapiService, private toast : ToastrService) {                                                                                                   
+    this.addblogform = this.detail.group({                                                                                                   
+      title: [''],                                                                                                   
+      summary: [''],                                                                                                    
+      description: [''], 
+      featured_RadioButton : '', 
+      categories: [0],                                                                                                  
+    });                                                                                                        
+  }
+  ngOnInit(){
+    this.getcategory();
+  }
+  onSubmit(): void {     
+    
+    console.log(this.addblogform.value, 'dddd')
+
+    this.StrapiService.addBlogForm(this.addblogform.value).subscribe(resp=>{
+      console.log(resp, 'RESP')
+    })
+    console.log('Your details have been submitted', this.addblogform.value);                                                                                                   
+    this.blogdetails.push(this.addblogform.value)                                                                                                   
+    console.log('this.blogdetails',this.blogdetails)
+    this.addblogform.reset();
+  }
+
+  getcategory(){
+    this.StrapiService.getCategoryData().subscribe(category_data => {
+      console.log(category_data,'data')
+      this.category = category_data;
+      Object.values(this.category.data).filter(x=>{
+        console.log(x,'x');
+        this.mydata.push(x);
+      })
+    },()=>{},
+    ()=>{
+      // this.addblogform = this.detail.group({
+        // categories: [0]
+      // });
+    }
+    );
+    console.log(this.mydata);
+  }
 }
