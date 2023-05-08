d42269acfcee3d625a4da54d6fe046e8aab9b243
maniar yash <maniaryash21@gmail.com>
2023-05-08 16:50:49 +0530

    completed footer Showed active subscribers.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog-app';
+
+  // @HostListener('window:close')
+  // ngOnInit() {
+  //   localStorage.removeItem('token')
+  // }
 }
